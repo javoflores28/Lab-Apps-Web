f48e9b8318e184f22378a79a6eb4f1a34d03a59c
Javier Flores <javoflores28@hotmail.com>
2022-03-23 01:21:42 -0600

    Tarea 2 - CRUD Bicicletas DB

--- CRUD-bicicletas-DB/models/bicicleta.js
+++ CRUD-bicicletas-DB/models/bicicleta.js
@@ -28,7 +28,7 @@ Bicicleta.add = (bicicleta) => {
 
 //Añadir un par de bicis:
 let b1 = new Bicicleta(1, 'rojo', 'bmx', [19.284770943610578, -99.13729060406136])
-let b2 = new Bicicleta(2, 'blanca', 'Benotto', [19.286055116801744, -99.1369912899661])
+let b2 = new Bicicleta(2, 'blanca', 'Benotto', [19.286055116811744, -99.1369912899661])
 
 Bicicleta.add(b1)
 Bicicleta.add(b2)
